80589c0e39cd6e32bc6639b57161c02ff0ff9383
Juan Ortiz <jmopr83@gmail.com>
2016-04-08 14:02:42 -0400

    Generate user model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ActiveRecord::Base
+  belongs_to :user
   validates :title, :company, :post_date, :description, presence: true
 
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408002833) do
+ActiveRecord::Schema.define(version: 20160408175915) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -22,5 +22,14 @@ ActiveRecord::Schema.define(version: 20160408002833) do
     t.datetime "updated_at",  null: false
     t.string   "company"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
