ee232ffff49ba2ff90bdabe235e075f7dc8c9f9f
Juan Ortiz <jmopr83@gmail.com>
2016-04-08 14:07:25 -0400

    Sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resource :users
 
   get 'jobs_controller/create'
 
