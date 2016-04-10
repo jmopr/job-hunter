a627d241c46a0bff6658c48fec290cb73215050b
Rodney Perez <RodneyePerez@gmail.com>
2016-04-09 21:06:34 -0400

    added logo validating method

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,14 @@
 class Job < ActiveRecord::Base
   belongs_to :user
   validates :title, :company, :post_date, :description, presence: true
-  
+
+  def logo_validator(url)
+    res = Faraday.get("https://logo.clearbit.com/#{url}")
+    unless res.status == 200
+      "https://placeholdit.imgix.net/~text?txtsize=33&txt=400%C3%97400&w=400&h=400"
+    else
+      "https://logo.clearbit.com/#{url}"
+    end
+  end
+
 end
