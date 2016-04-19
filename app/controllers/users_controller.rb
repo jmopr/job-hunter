fdb5ca3c3600ce9fe8c30aaac7923f3fb239d496
Juan Ortiz <jmopr83@gmail.com>
2016-04-19 00:46:07 -0400

    Added update resume feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if current_user.update(user_params)
+        format.html { redirect_to users_jobs_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :password, :password_confirmation, :first_name,
